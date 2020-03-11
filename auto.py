6809889025737555d234342ac30715612ae7c2ce
Prateek Senapati <prateek1210s@gmail.com>
2020-03-11 10:35:02 +0530

    Add auto.py

---
+++ auto.py
@@ -0,0 +1,11 @@
+# Script to launch Google Chrome, Atom IDE and Jupyter Notebook
+
+import os
+
+def main():
+    os.startfile(r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk')
+    os.startfile(r'C:\Users\HP\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\GitHub, Inc\Atom.lnk')
+    os.startfile(r'C:\Users\HP\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Jupyter Notebook (Anaconda3).lnk')
+
+if __name__ == '__main__':
+    main()
