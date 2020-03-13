fbdadc530782266c8e8f4742a74ff4973a3ef808
Prateek Senapati <prateek1210s@gmail.com>
2020-03-13 22:43:04 +0530

    Change paths to web browser and IDE

--- auto.py
+++ auto.py
@@ -3,9 +3,20 @@
 import os
 
 def main():
-    os.startfile(r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk')
-    os.startfile(r'C:\Users\HP\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\GitHub, Inc\Atom.lnk')
-    os.startfile(r'C:\Users\HP\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Jupyter Notebook (Anaconda3).lnk')
+    # browser_path contains the path to the web browser (here, Google Chrome) executable file
+    browser_path = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+
+    # ide_path contains the path to the IDE (here, Atom IDE) executable file
+    ide_path = r'C:\Users\HP\AppData\Local\atom\atom.exe'
+
+    # jupyter_nb_path contains the path to the Jupyter Notebook shortcut/link to the original executable file
+    jupyter_nb_path = r'C:\Users\HP\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Jupyter Notebook (Anaconda3).lnk'
+
+    # The aforementioned path variables can also contain paths to shortcuts/links to the respective executable files
+
+    os.startfile(browser_path)
+    os.startfile(ide_path)
+    os.startfile(jupyter_nb_path)
 
 if __name__ == '__main__':
     main()
