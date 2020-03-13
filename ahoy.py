b5336bc604e12fdc6253a335a6d94c835362e7d1
Prateek Senapati <prateek1210s@gmail.com>
2020-03-13 23:01:47 +0530

    Add path to cloned repository to generalize the code

--- ahoy.py
+++ ahoy.py
@@ -1,11 +1,12 @@
 # This driver script performs the following tasks
-# (1) Move to the directory containing the web scraping and automation scripts, viz. barca.py and auto.py
+# (1) Move to the cloned directory containing the web scraping and automation scripts, viz. barca.py and auto.py
 # (2) Execute the aforementioned scripts
 
 import os
 
 def main():
-    os.chdir(r'C:\Users\HP\Documents\ATOM C++\Python 3 Bootcamp\ws_api_am\automation_scripts')
+    clone_path = r'C:\Users\HP\Ahoy' # clone_path contains the path to the cloned repository
+    os.chdir(clone_path)
     os.system(r'python auto.py')
     os.system(r'python barca.py')
 
